81bdca8918e56aa2ea300f6b5944bd917fff1ac8
Thu Ya Oo <tyo@mit.com.mm>
2023-12-07 22:12:22 +0630

    Error Handling

--- repository/UserDao.js
+++ repository/UserDao.js
@@ -13,32 +13,35 @@ exports.findById = async (id) => {
 
 exports.createUser = async (user) => {
     const createUser = await db.query('INSERT INTO users.user (id ,name ,username ,password ,nrc ,'+
-    'email ,phno ,user_type ,dob ,gender ,address) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING * '
+    'email ,phno ,user_type ,dob ,gender ,address) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)'
     ,[user.id,user.name,user.username,user.password,user.nrc,user.email,user.phno,user.user_type,user.dob,user.gender,user.address],
-    (error,result) => {
-        if(error){
-            console.error('Error in Executing INSERT Query ',error);
-        }
-    });
-    return createUser.rows[0];
+    // (error,result) => {
+    //     if(error){
+    //         console.error('Error in Executing INSERT Query ',error);
+    //     }
+    // }
+    );
+    return createUser;
 }
 
 exports.updateUser = async(user,id) => {
     const updatedUser = await db.query('UPDATE users.user SET name = $1 WHERE id = $2',[user.name,id],
-    (error,result) => {
-        if(error){
-            console.error('Error in Executing Update Query ',error);
-        }
-    }) 
+    // (error,result) => {
+    //     if(error){
+    //         console.error('Error in Executing Update Query ',error);
+    //     }
+    // }
+    ) 
     return updatedUser;
 }
 
 exports.deleteUser = async(id) => {
     const deletedUser = await db.query('DELETE FROM users.user WHERE id = $1',[id],
-    (error,result) => {
-        if(error){
-            console.error('Error in Executing Update Query ',error);
-        }
-    })
+    // (error,result) => {
+    //     if(error){
+    //         console.error('Error in Executing Update Query ',error);
+    //     }
+    // }
+    )
     return deletedUser;
 }
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const pool = require("./db");
 const userRoute = require("./route/userRoute");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.listen(PORT,() => {console.log(`Server is running on Port ${PORT}`)}); 
 
-app.use(express.json());
+app.use(express.json({limit: "1gb"}));
+app.use(cors());
 app.use('/api', userRoute);
 
 // Test the connection
